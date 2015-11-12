8096dfb2e79a3f4caa6f182f04cd74a2848a0f39
Christoph Schulze <chr_schulze@freenet.de>
2015-11-12 14:57:07 +0100

    marked ToDoListMB as managed bean

--- src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/ToDoListMB.java
+++ src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/ToDoListMB.java
@@ -1,26 +1,35 @@
 package de.htw_berlin.ai_bachelor.kbe.checklist.mb;
 
-import java.io.Serializable;
-
 import de.htw_berlin.ai_bachelor.kbe.checklist.model.ToDoList;
 
-//TODO
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "toDoList", eager = true)
+@SessionScoped
 public class ToDoListMB implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private ToDoList toDoList;
-    
-	public ToDoListMB() {
-		super();
-		//TODO
-	}    
 
-	//TODO
+    public ToDoListMB() {
+        super();
+        toDoList = new ToDoList();
+    }
 
-	//Should be called if the Button "Speichern" is pushed.
-	//Needs configuration in the faces-config.xml.
+    public ToDoList getToDoList() {
+        return toDoList;
+    }
+
+    public void setToDoList(ToDoList toDoList) {
+        this.toDoList = toDoList;
+    }
+
+    //Should be called if the Button "Speichern" is pushed.
+    //Needs configuration in the faces-config.xml.
     public String save() {
-    	return "save";
+        return "save";
     }
 }
