396640d9845b959d50c584523476b87f5685273e
Christoph Schulze <chr_schulze@freenet.de>
2016-01-20 14:37:36 +0100

    set TimerMB to application scope

--- src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/TimerMB.java
+++ src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/TimerMB.java
@@ -1,7 +1,7 @@
 package de.htw_berlin.ai_bachelor.kbe.checklist.mb;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,7 +9,7 @@ import java.util.Date;
  * Created by cschulze on 17.12.2015.
  */
 @ManagedBean(name = "Timer", eager = true)
-@RequestScoped
+@ApplicationScoped
 public class TimerMB {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
